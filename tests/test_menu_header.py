8961fa1448e4abc927d335f0eb1652e24cf53753
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2026-01-19 14:04:23 +0100

    Refactor menu header tests for improved clarity
    Refactor test to use a list of tuples for menu links and URLs. Simplify click handling for mobile and desktop devices, ensuring correct URL verification after navigation.

--- tests/test_menu_header.py
+++ tests/test_menu_header.py
@@ -11,25 +11,28 @@ def test_menu_header(page: Page, device_name):
 
     print("AND acepta las cookies")
     page.get_by_role("button", name="Aceptar todo").click()
+    #bien los enlaces
+    enlaces_menu = [
+    ("Vacaciones", "https://www.viajerospiratas.es/viajes/vacaciones"),
+    ("Chollos", "https://www.viajerospiratas.es/viajes"),
+    ("Fin de semana", "https://www.viajerospiratas.es/viajes/fin-de-semana"),
+]
 
-    enlaces_menu = ["Vacaciones", "Chollos", "Fin de semana"]
-
-    for enlace in enlaces_menu:
-
+    for enlace, url in enlaces_menu:
+        #aqui si es movil, hace clic en el menu hamburguesa, si es escritorio simplemente no hace nada porque no tiene que hacer clic en menu hamburguesa
         if device_name == "mobile":
             print("WHEN el usuario abre el menú hamburguesa")
             page.locator("header button").first.click()
-            page.wait_for_timeout(500)
-
-            print(f"AND hace click en '{enlace}' (móvil)")
-            page.get_by_role("link", name=enlace).first.click(force=True)
-
-        else:
-            print(f"AND hace click en '{enlace}' (escritorio)")
-            page.get_by_role("link", name=enlace).first.click()
-
+        #aqui tanto en movil como en escritorio hacen lo mismo asi que lo dejas fuera del if, simplemente hace click en el enlace y ya.
+        print(f"AND hace click en '{enlace}'")
+        page.get_by_role("link", name=enlace).first.click(force=True)
+        #aqui estas comprobando solo que aparece el boton, pero en realidad debes comprobar que la url cambia entonces quita esto:
         print("THEN el enlace responde correctamente")
         assert page.get_by_role("link", name=enlace).first.is_visible()
-
+        #en su lugar pon esto otro, esperas que carge la url nueva al hacer clic en el menu y luego compruebas que es la esperada
+        print("THEN el enlace es el correcto")
+        page.wait_for_url(url)
+        assert page.url == url
+        #esto lo puedes quitar porque no te hace falta volver a la principal ya que los botones del menu siguen visibles en todas las paginas asi que puedes cambiar de uno a otro sin tener que volver a la principal
         print("AND vuelve a la página principal")
         page.goto("https://www.viajerospiratas.es/")
